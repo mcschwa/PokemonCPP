31d4fe42553023771d3717307542bbd90c4b3bf0
Rick Huisman <rickhousemen2@gmail.com>
2023-02-14 18:10:04 +0100

    removed nonsense

--- PokemonCPP/Pokemon.h
+++ PokemonCPP/Pokemon.h
@@ -28,54 +28,6 @@ namespace Pokemons
 		Fairy
 	};
 
-	Type getRandomType()
-	{
-		return Type(rand() % 10);
-	}
-
-	string typeToString(Type _type)
-	{
-		switch (_type)
-		{
-		case Pokemons::Normal:
-			return "Normal";
-		case Pokemons::Fire:
-			return "Fire";
-		case Pokemons::Water:
-			return "Water";
-		case Pokemons::Grass:
-			return "Grass";
-		case Pokemons::Flying:
-			return "Flying";
-		case Pokemons::Fighting:
-			return "Fighting";
-		case Pokemons::Poison:
-			return "Poison";
-		case Pokemons::Electric:
-			return "Electric";
-		case Pokemons::Ground:
-			return "Ground";
-		case Pokemons::Rock:
-			return "Rock";
-		case Pokemons::Psychic:
-			return "Psychic";
-		case Pokemons::Ice:
-			return "Ice";
-		case Pokemons::Bug:
-			return "Bug";
-		case Pokemons::Ghost:
-			return "Ghost";
-		case Pokemons::Steel:
-			return "Steel";
-		case Pokemons::Dragon:
-			return "Dragon";
-		case Pokemons::Dark:
-			return "Dark";
-		case Pokemons::Fairy:
-			return "Fairy";
-		}
-	}
-
 	class Attack
 	{
 	public:
