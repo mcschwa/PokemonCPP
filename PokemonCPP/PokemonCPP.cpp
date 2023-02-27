9b1a3e1229f420c2cf850fef0daafea7949da197
Rick Huisman <rickhousemen2@gmail.com>
2023-02-27 15:33:45 +0100

    finally figured out how to do inline arrays.

--- PokemonCPP/PokemonCPP.cpp
+++ PokemonCPP/PokemonCPP.cpp
@@ -11,14 +11,11 @@ using namespace Attacks;
 
 int main()
 {
-	Type weaknessesC[2] = { Water, Water };
-	Type resistancesC[2] = { Fire, Flying };
 	Type weaknessesW[2] = { Fire, Fire };
 	Type resistancesW[2] = { Water, Flying };
-	Attack AttacksF[2] = { firePunch, fireSwing };
 	Attack AttacksW[2] = { waterPunch, waterSwing };
 
-	Pokemon Charmender("Charmender", Fire, 40, weaknessesC, resistancesC, AttacksF);
+	Pokemon Charmender("Charmender", Fire, 40, new Type[2]{Water, Fire}, new Type[2] {Fire, Flying}, new Attack[2] {firePunch, fireSwing});
 	Pokemon Watermon("Watermon", Water, 40, weaknessesW, resistancesW, AttacksW);
 
 	Charmender.attackEnemy(Watermon, Charmender.pickAttack());
